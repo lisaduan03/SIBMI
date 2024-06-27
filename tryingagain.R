bf28a5b5e3c8139cef304d24d4eda3ce1dce6d04
lisaduan03 <lisaduan03@gmail.com>
2024-06-27 12:59:31 -0400

    reproducing Diao 2020 from 6/20. can only achieve same medians if impute, which seems wrong

--- tryingagain.R
+++ tryingagain.R
@@ -15,7 +15,7 @@ Demo_1516 <- nhanesTranslate('DEMO_I', names(demo_1516), data=demo_1516)
 nhanesTables(data_group='LAB', year=2015)
 biopro_1516 <- nhanes("BIOPRO_I")
 # selecting columns
-Demo_1516select <- Demo_1516[c("SEQN", "RIDEXPRG", "RIAGENDR", "RIDAGEYR", "RIDRETH3", "SDMVPSU", "SDMVSTRA", "WTMEC2YR", "SDDSRVYR", "RIDSTATR")]
+Demo_1516select <- Demo_1516[c("SEQN", "RIDEXPRG", "RIAGENDR", "RIDAGEYR", "RIDRETH3", "SDMVPSU", "SDMVSTRA", "WTMEC2YR", "SDDSRVYR")]
 biopro_1516select <- biopro_1516[c("SEQN", "LBXSCR")]
 # merging on ID (SEQN)
 merged_1516 <- merge(Demo_1516select, biopro_1516select, by = "SEQN", all = TRUE)
@@ -28,57 +28,52 @@ demo_1718 <- nhanes("DEMO_J")
 Demo_1718 <- nhanesTranslate('DEMO_J', names(demo_1718), data=demo_1718)
 nhanesTables(data_group='LAB', year=2017)
 biopro_1718 <- nhanes("BIOPRO_J")
-Demo_1718select <- Demo_1718[c("SEQN", "RIDEXPRG", "RIAGENDR", "RIDAGEYR", "RIDRETH3", "SDMVPSU", "SDMVSTRA", "WTMEC2YR", "SDDSRVYR", "RIDSTATR")]
+Demo_1718select <- Demo_1718[c("SEQN", "RIDEXPRG", "RIAGENDR", "RIDAGEYR", "RIDRETH3", "SDMVPSU", "SDMVSTRA", "WTMEC2YR", "SDDSRVYR")]
 biopro_1718select <- biopro_1718[c("SEQN", "LBXSCR")]
 merged_1718 <- merge(Demo_1718select, biopro_1718select, by = "SEQN", all = TRUE)
-head(merged_1718)
 
-# concatenated and display
+# Concatenate the two datasets
 merged_data_combined <- rbind(merged_1516, merged_1718)
+
+# Display the combined data
 head(merged_data_combined)
 
-# See how many missing LBXSCR values there are. Paper didn't do any imputation
-missing_count <- sum(is.na(merged_data_combined$LBXSCR))
-print(paste("Number of missing serum creatinine values:", missing_count))
-print(paste("Number of total people:", nrow(merged_data_combined)))
-print(paste("Response rate:", 1 - (missing_count/nrow(merged_data_combined))))
+# Get a summary
+summary(merged_data_combined)
+# missing_indices <- which(is.na(merged_data_combined$LBXSCR))
 
-### Survey Weights ####
-# "This design object should be created before any subsetting or manipulation of the data" cran.r-project
-# "a good rule of thumb is to use "the least common denominator" -- from weighting module, so we use MEC not INT
-nhanesDesign <- svydesign(id = ~SDMVPSU,  # Primary Sampling Units (PSU)
-                          strata  = ~SDMVSTRA, # Stratification used in the survey
-                          weights = ~ifelse(SDDSRVYR %in% c(9, 10), 0.5 * WTMEC2YR, WTMEC2YR),  # according to CDC Weighting Module
-                          nest    = TRUE,      # Whether PSUs are nested within strata
-                          data    = merged_data_combined)
-### response rates ###
-missing_count <- sum(is.na(datasub$LBXSCR))
-print(paste("Number of missing serum creatinine values:", missing_count))
-print(paste("Number of total people:", nrow(datasub)))
-print(paste("Response rate:", 1 - (missing_count/nrow(datasub))))
+# Mean imputation
+# mean_cre <- mean(merged_data_combined$LBXSCR, na.rm = TRUE)
+# merged_data_combined$LBXSCR[missing_indices] <- mean_cre
 
-### filtering ####
-dfsub <- subset(nhanesDesign , RIDSTATR %in% "Both interviewed and MEC examined") # this line useless, caught by next line
-dfsub <- subset(dfsub, complete.cases(LBXSCR) 
-                & !RIDEXPRG %in% c("Yes, positive lab pregnancy test or self-reported pregnant at exam")
-                & RIDAGEYR > 18)
-
-merged_data_sub <- subset(merged_data_combined , RIDSTATR %in% "Both interviewed and MEC examined") # this line useless, caught by next line
-merged_data_sub <- subset(merged_data_combined, complete.cases(LBXSCR) 
-                & !RIDEXPRG %in% c("Yes, positive lab pregnancy test or self-reported pregnant at exam")
-                & RIDAGEYR > 18)
-
-
-merged_data_combined <- merged_data_combined %>%
-  filter(!RIDEXPRG %in% c("Yes, positive lab pregnancy test or self-reported pregnant at exam"))
-
-# The CKD-EPI equation from Levey et al., 2009
-# if with_race == True, then include race in calculation
-calculate_eGFR <- function(creatinine, age, sex, race, with_race) {
-  if (any(is.na(c(creatinine, age, sex, race, with_race)))) {
+#### [lltting] ###
+calculate_eGFR_no_race <- function(creatinine, age, sex) {
+  if (is.na(creatinine) | is.na(age) | is.na(sex)) {
     return(NA)
   }
-  
+    
+  if (sex == 1) {  # Male
+    if (creatinine <= 0.9) {
+      eGFR <- 141 * (creatinine / 0.9) ^ -0.411 * 0.993 ^ age
+    } else {
+      eGFR <- 141 * (creatinine / 0.9) ^ -1.209 * 0.993 ^ age
+    }
+  } else {  # Female
+    if (creatinine <= 0.7) {
+      eGFR <- 144 * (creatinine / 0.7) ^ -0.329 * 0.993 ^ age
+    } else {
+      eGFR <- 144 * (creatinine / 0.7) ^ -1.209 * 0.993 ^ age
+    }
+  }
+  return(eGFR)
+}
+
+# Calculate eGFR
+calculate_eGFR <- function(creatinine, age, sex, race) {
+  if (is.na(creatinine) | is.na(age) | is.na(sex)) {
+    return(NA)
+  }
+    
   if (sex == 1) {  # Male
     if (creatinine <= 0.9) {
       eGFR <- 141 * (creatinine / 0.9) ^ -0.411 * 0.993 ^ age
@@ -93,20 +88,27 @@ calculate_eGFR <- function(creatinine, age, sex, race, with_race) {
     }
   }
   # Adjust for Black race
-  if (race == "Non-Hispanic Black" && with_race == TRUE) {
+  if (race == "Non-Hispanic Black") {
+    print("NHB!!!!!")
     eGFR <- eGFR * 1.159
   }
   return(eGFR)
 }
 
-### adding columns to the raw data and weighted
-merged_data_combined<- merged_data_combined %>%
-  mutate(
-    eGFR = mapply(calculate_eGFR, creatinine = LBXSCR, age = RIDAGEYR, sex = RIAGENDR, race = RIDRETH3, with_race = TRUE),
-    eGFR_no_race = mapply(calculate_eGFR, creatinine = LBXSCR, age = RIDAGEYR, sex = RIAGENDR, race = RIDRETH3, with_race = FALSE),
-    change_in_eGFR = eGFR_no_race - eGFR
-  )
+merged_data_combined$eGFR <- mapply(calculate_eGFR, 
+                                    creatinine = merged_data_combined$LBXSCR, 
+                                    age = merged_data_combined$RIDAGEYR, 
+                                    sex = merged_data_combined$RIAGENDR, 
+                                    race = merged_data_combined$RIDRETH3)
 
+# Apply eGFR calculation without race adjustment
+merged_data_combined$eGFR_no_race <- mapply(calculate_eGFR_no_race, 
+                                            creatinine = merged_data_combined$LBXSCR, 
+                                            age = merged_data_combined$RIDAGEYR, 
+                                            sex = merged_data_combined$RIAGENDR)
+
+merged_data_combined <- merged_data_combined %>%
+  mutate(change_in_eGFR = eGFR_no_race - eGFR)
 
 # Calculate mean eGFR without race adjustment
 mean_egfr_no_race <- merged_data_combined %>%
@@ -146,65 +148,37 @@ plot_with_race <- ggplot(merged_data_combined, aes(x = factor(RIDRETH3), y = eGF
 grid.arrange(plot_no_race, plot_with_race, ncol = 2)
 
 
-merged_data_sub_black <- merged_data_sub %>%
-  filter(RIDRETH3 == "Non-Hispanic Black")
-mean_black_eGFR = mean(merged_data_sub_black$eGFR)
-print(mean_black_eGFR)
+nhanesDesign <- svydesign(id = ~SDMVPSU,  # Primary Sampling Units (PSU)
+                          strata  = ~SDMVSTRA, # Stratification used in the survey
+                          weights = ~ifelse(SDDSRVYR %in% c(9, 10), 0.5 * WTMEC2YR, WTMEC2YR),  # according to CDC Weighting Module
+                          nest    = TRUE,      # Whether PSUs are nested within strata
+                          data    = merged_data_combined)
 
-mean_egfr <- merged_data_combined %>%
-  group_by(RIDRETH3) %>%
-  summarise(mean_eGFR = mean(merged_data_combined$eGFR, na.rm = TRUE))
-print(mean_egfr)
+datasub <- merged_data_combined %>%
+  filter(RIDRETH3 == "Non-Hispanic Black" & 
+           !RIDEXPRG %in% c("Cannot ascertain if the participant is pregnant at exam",
+                            "Yes, positive lab pregnancy test or self-reported pregnant at exam"))
 
-
-dfsub_black = subset(nhanesDesign, RIDRETH3 == "Non-Hispanic Black")
-weighted_median_change_in_eGFR <- svyquantile(~ dfsub_black$change_in_eGFR, design = dfsub_black, quantiles = 0.75)
-print(weighted_median_change_in_eGFR)
-
-dfsub <- update(dfsub, 
-                       eGFR = mapply(calculate_eGFR, creatinine = LBXSCR, age = RIDAGEYR, sex = RIAGENDR, race = RIDRETH3, with_race = TRUE),
-                       eGFR_no_race = mapply(calculate_eGFR, creatinine = LBXSCR, age = RIDAGEYR, sex = RIAGENDR, race = RIDRETH3, with_race = FALSE),
-                       change_in_eGFR = eGFR_no_race - eGFR
-)
-
-
-######## Seeing impact of weighting ########
-library(dplyr)
-library(gridExtra)
-# Calculate mean eGFR without race adjustment
-mean_egfr_no_race <- sapply(split(merged_data_sub$eGFR_no_race, merged_data_sub$RIDRETH3), mean, na.rm=TRUE) # this should calculate across all non-NA values? 
-mean_egfr_no_race_adj <- svyby(merged_data_sub$eGFR_no_race, ~ RIDRETH3, dfsub, svymean, na.rm=TRUE)
-mean_egfr_no_race_adj <- mean_egfr_no_race_adj$V1
-mean_egfr <- sapply(split(merged_data_sub$eGFR, merged_data_sub$RIDRETH3), mean, na.rm=TRUE)
-mean_egfr_adj <- svyby(merged_data_sub$eGFR, ~ RIDRETH3, dfsub, svymean, na.rm=TRUE)
-mean_egfr_adj <- mean_egfr_adj$V1
-
-
-eGFR_averages <- data.frame(
-  RIDRETH3 = names(mean_egfr_no_race),  # Use names as RIDRETH3 values
-  "No_Race_Unweighted" = as.numeric(mean_egfr_no_race),  # Convert to numeric values
-  "No_Race_Weighted" = mean_egfr_no_race_adj, 
-  "With_Race_Unweighted" = as.numeric(mean_egfr),
-  "With_Race_Weighted" = mean_egfr_adj
-)
-library(tidyr)
-eGFR_averages_long <- gather(eGFR_averages, key = "Measurement", value = "Value", 
-                             No_Race_Unweighted, No_Race_Weighted, 
-                             With_Race_Unweighted, With_Race_Weighted)
-
-ggplot(eGFR_averages_long, aes(x = RIDRETH3, y = Value, fill = Measurement)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  theme_minimal() +
-  labs(title = "eGFR Averages by Ethnicity",
-       x = "Race/Ethnicity",
-       y = expression(eGFR ~ (mL/min/1.73 ~ m^2)),
-       fill = "With or Without Race/Weight") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 ###### Recreating Figure 1 in Diao et al., 2020 JAMA Paper ######
+datasub <- merged_data_combined %>%
+  filter(RIDRETH3 == "Non-Hispanic Black" & 
+           !RIDEXPRG %in% c("Cannot ascertain if the participant is pregnant at exam",
+                            "Yes, positive lab pregnancy test or self-reported pregnant at exam"))
+
+missing_count <- sum(is.na(datasub$LBXSCR))
+print(paste("Number of missing serum creatinine values:", missing_count))
+print(paste("Number of total people:", nrow(datasub)))
+print(paste("Response rate:", 1 - (missing_count/nrow(datasub))))
+
+# create a subset of nhanesDesign for NHB and nonpregnant adults
+dfsub <- subset(nhanesDesign, RIDRETH3 == "Non-Hispanic Black" & 
+                  !RIDEXPRG %in% c("Cannot ascertain if the participant is pregnant at exam",
+                                   "Yes, positive lab pregnancy test or self-reported pregnant at exam"))
+
 
 # without survery weights
-ggplot(merged_data_sub, aes(x = change_in_eGFR)) +
+ggplot(datasub, aes(x = change_in_eGFR)) +
   geom_histogram(binwidth = 1, fill = "salmon", color = "black", alpha = 0.7) +
   labs(x = expression(paste("change in eGFR ", (mL/min/1.73 ~ m^2), " (no race - race)")),
        y = "Black adults in NHANES (2015-2018)",
@@ -223,6 +197,7 @@ svyhist(~ change_in_eGFR, design = dfsub,
         border = "black",
         cex.main = 0.8)
 
+
 ######## Table 1 ########
 # function that takes in cutoff ranges and implication name, creates row in a table
 create_summary_table <- function(cutoff_ranges, implications) {
@@ -236,20 +211,20 @@ create_summary_table <- function(cutoff_ranges, implications) {
     upper_cutoff <- cutoff_range[2]
     
     # Weighted: proportions with race
-    prop_and_se_with_race <- svyby(~(eGFR >= lower_cutoff & eGFR < upper_cutoff), ~RIDRETH3, design = dfsub, svymean, na.rm = TRUE) # mean of many 1s and 0s = proportion 
+    prop_and_se_with_race <- svyby(~(eGFR >= lower_cutoff & eGFR < upper_cutoff), ~RIDRETH3, design = nhanesDesign, svymean, na.rm = TRUE) # mean of many 1s and 0s = proportion 
     prop_with_race <- prop_and_se_with_race[prop_and_se_with_race$RIDRETH3 == "Non-Hispanic Black", "eGFR >= lower_cutoff & eGFR < upper_cutoffTRUE"]
     se_with_race <- prop_and_se_with_race[prop_and_se_with_race$RIDRETH3 == "Non-Hispanic Black", "se.eGFR >= lower_cutoff & eGFR < upper_cutoffTRUE"]
     
     # Weighted:  proportions without race
-    prop_and_se_without_race <- svyby(~(eGFR_no_race >= lower_cutoff & eGFR_no_race < upper_cutoff), ~RIDRETH3, design = dfsub, svymean, na.rm = TRUE)
+    prop_and_se_without_race <- svyby(~(eGFR_no_race >= lower_cutoff & eGFR_no_race < upper_cutoff), ~RIDRETH3, design = nhanesDesign, svymean, na.rm = TRUE)
     prop_without_race <- prop_and_se_without_race[prop_and_se_without_race$RIDRETH3 == "Non-Hispanic Black", "eGFR_no_race >= lower_cutoff & eGFR_no_race < upper_cutoffTRUE"]
     se_without_race <- prop_and_se_without_race[prop_and_se_without_race$RIDRETH3 == "Non-Hispanic Black", "se.eGFR_no_race >= lower_cutoff & eGFR_no_race < upper_cutoffTRUE"]
     
     # Unweighted: counts with and without race
-    count_with_race <- merged_data_sub %>%
+    count_with_race <- datasub %>%
       summarise(n = sum(eGFR >= lower_cutoff & eGFR < upper_cutoff, na.rm = TRUE))
     
-    count_without_race <- merged_data_sub %>%
+    count_without_race <- datasub %>%
       summarise(n = sum(eGFR_no_race >= lower_cutoff & eGFR_no_race < upper_cutoff, na.rm = TRUE))
     
     # Weighted: Calculate absolute change in proportions
@@ -273,7 +248,7 @@ create_summary_table <- function(cutoff_ranges, implications) {
       "Removing Race" = paste0(count_without_race$n, " (", format(prop_without_race * 100, nsmall = 2), ")"),
       "Absolute change, weighted % (95% CI)" = paste0(format(absolute_change * 100, nsmall = 2),
                                                       " (", format(ci_low * 100, nsmall = 2), ", ", format(ci_high * 100, nsmall = 2), ")")    )
-
+    
     
     # Append current data table to list
     all_tables[[i]] <- data_table
@@ -297,14 +272,3 @@ print(summary_table)
 myTable <- tableGrob(summary_table)
 library(grid)
 grid.draw(myTable)
-
-# controls number of digits
-options(digits=2)
-
-
-####### Additional testing to make sure Table 1 is accurate, since slightly off from DIAO) #########
-nhanes_srvyr_design <- as_survey(dfsub)
-nhanes_srvyr_design %>%
-  group_by(RIDRETH3) %>%
-  summarize(proportion = survey_mean(eGFR_no_race < 60, na.rm = TRUE))
-
